dcddf4ca8b5fe9d09df3ee231416049e20ecaea4
parkminwoo <parkminwoo1991@gmail.com>
2023-04-06 20:00:17 +0900

    feat: update chatbot config

---
+++ chatbot/config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    chat_params = {"gpt_tok":"danielpark/medical-QA-chatGPT2-tok-v1",
+                   "tf_gpt_model":"danielpark/medical-QA-chatGPT2-v1",
+                   "bert_tok":"danielpark/medical-QA-BioRedditBERT-uncased-v1",
+                   "tf_q_extractor": "question_extractor_model_v1",
+                   "data":"danielpark/MQuAD-v1",
+                   "max_answer_len": 20,
+                   "isEval": False}
