e12921da7215f1935ab66e1448b4a3cad203b2b0
parkminwoo <parkminwoo1991@gmail.com>
2023-04-07 15:27:51 +0900

    streamlit: landing page sample

--- chatbot.py
+++ chatbot.py
@@ -16,6 +16,16 @@ import math
 import pandas as pd
 import streamlit as st
 
+"""
+# Welcome to Streamlit!
+
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
 with st.echo(code_location='below'):
    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
