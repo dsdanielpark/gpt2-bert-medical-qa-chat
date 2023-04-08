757db423a14d427df6617aebd35f0c68d0f0b264
parkminwoo <parkminwoo1991@gmail.com>
2023-04-08 11:59:57 +0900

    refactor: dir name changed

--- module/chatbot/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    chat_params = {"gpt_tok":"danielpark/medical-QA-chatGPT2-tok-v1",
-                   "tf_gpt_model":"danielpark/medical-QA-chatGPT2-v1",
-                   "bert_tok":"danielpark/medical-QA-BioRedditBERT-uncased-v1",
-                   "tf_q_extractor": "question_extractor_model",
-                   "data":"danielpark/MQuAD-v1",
-                   "max_answer_len": 20,
-                   "isEval": False}
